55a3b57a13518b5698a43118567ec7e2bb25388b
root <tefeglory.official@gmail.com>
2023-12-05 17:31:09 +0100

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ module for Base model that defines all common attributes/methods
 for other classes
 """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -22,13 +22,18 @@ class BaseModel:
     to_dict(self): returns a dict containing all keys/values of the instance
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
 
-        for key, value in kwargs.items():
-            self[key] = value
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if (key != "__class__"):
+                    if (key == "updated_at" or key == "created_at"):
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ returns  [<class name>] (<self.id>) <self.__dict__>"""
